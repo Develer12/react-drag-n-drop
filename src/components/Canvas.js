1c34d0dba15ed8b88d04ffb19ce8388210295a65
Develer12 <cenia-v@mail.ru>
2020-08-04 16:01:48 +0300

    Add drag in canvas

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -16,55 +16,70 @@ function Canvas(props) {
     }
 
     const drop = (e) => {
-            e.preventDefault();
-            const fig_id = e.dataTransfer.getData('fig_id');
-            
-            console.log(fig_id)
+        e.preventDefault();
+        const fig_id = e.dataTransfer.getData('fig_id');
+        
 
-            const fig = document.getElementById(fig_id);
+        const fig = document.getElementById(fig_id);
 
+        let id = randomId();
+        while(document.getElementById(id)){
+            id = randomId();
+        }
 
-            let id = randomId();
-            while(document.getElementById(id)){
-                id = randomId();
+        let inArea = document.getElementById('canvasArea');
+        inArea = inArea.querySelector(`[id='${fig.id}']`);
+
+        //let canvasArea = document.getElementById('canvasArea');
+        let pageX = e.pageX, pageY = e.pageY;
+
+        let style = {
+            left: pageX - figureW/2,
+            top: pageY - figureH/2,
+            //left: `${ ((pageX - figureW/2) / canvasArea.offsetWidth * 100).toFixed(1)}%`, 
+            //top: `${ ((pageY - figureH/2) / canvasArea.offsetHeight * 100).toFixed(1)}%`
+        };
+
+        if(fig && id !== fig.id){
+
+            if(fig.parentNode.id === 'canvasArea'){
+                let index = figOnCanvas.findIndex((item) => `figure-${item.id}` === fig.id);
+                figOnCanvas[index].style = style;
+                console.log(figOnCanvas[index].className)
             }
-
-            let inArea = document.getElementById('canvasArea');
-            inArea = inArea.querySelector(`[id='${fig.id}']`);
-
-            //let canvasArea = document.getElementById('canvasArea');
-            let pageX = e.pageX, pageY = e.pageY;
-
-            let style = {
-                left: pageX - figureW/2,
-                top: pageY - figureH/2,
-                //left: `${ ((pageX - figureW/2) / canvasArea.offsetWidth * 100).toFixed(1)}%`, 
-                //top: `${ ((pageY - figureH/2) / canvasArea.offsetHeight * 100).toFixed(1)}%`
-            };
-
-            if(fig && id !== fig.id && !inArea){
+            else{
                 figOnCanvas.push({ 
                     id: id, 
                     draggable: fig.draggable, 
                     style: style, 
                     className: fig.className 
                 });
-                
-                const figures = [];
-                figOnCanvas.map((item) => (
-                    figures.push(React.createElement(
-                        Figure, { 
-                            id: item.id, 
-                            draggable: item.draggable,
-                            style: item.style, 
-                            className: item.className.indexOf('invisible')? 
-                                item.className.replace(/invisible/g, 'visible') : item.className
-                        })
-                    )
-                ));
-
-                ReactDOM.render(figures, e.target);
             }
+
+            const figures = [];
+            figOnCanvas.map((item) => (
+                figures.push(React.createElement(
+                    Figure, { 
+                        id: item.id, 
+                        draggable: item.draggable,
+                        style: item.style, 
+                        className: item.className
+                    })
+                )
+            ));
+
+            ReactDOM.render(figures, e.target);
+
+            setTimeout(() => {
+                let invisible = document.getElementById('canvasArea');
+                invisible = invisible.getElementsByClassName('invisible')[0];
+                if(invisible){
+                    invisible.classList.remove('invisible');
+                    invisible.classList.add('visible');
+                }
+            }, 0);
+        }
+        
     }
 
     const dragOver = (e) => {
